92850e5586317097152f0485dbf431a544a7f83c
ernesto13 <efsamaniego@me.com>
2018-02-16 18:19:23 -0700

    changed port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 
-var PORT = 3003;
+var PORT = 3000;
 var app = express();
 
 
